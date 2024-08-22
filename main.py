9566054a9a5aa9c00ffcda99fb25d944606d7554
Medwin <garmed172@gmail.com>
2024-08-22 11:48:32 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1------------------------------------------------------------
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+
+
+if num1 > num2:
+    print(num1 + " is greater than " + num2)
+
+elif num1 < num2:
+    print(num1 + " is less than " + num2)
+
+elif num1 == num2:
+    print(num1 + " is equals to " + num2)
+
+# 2------------------------------------------------------------
+
+# name1 = input("Enter classmate name 1: ")
+# name2 = input("Enter classmate name 2: ")
+# name3 = input("Enter classmate name 3: ")
+#
+# print("Your classmates are " + name1 + ", " + name2 + " & " + name3 + ".")
+
